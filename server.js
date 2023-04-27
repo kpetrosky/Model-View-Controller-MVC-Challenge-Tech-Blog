63e7ab2001d435d25b2cd83f0011179dafaf8ce9
kpetrosky <kpetrosky36@gmail.com>
2023-04-26 21:03:07 -0700

    added readme with user/acceptance and readme template, added server.js file, and added potiential down needs

---
+++ server.js
@@ -0,0 +1,10 @@
+//npm i inquirer@8.2.4
+//npm i dotenv mysql2 console.table
+//npm init -y
+//mysql -u root -p
+//SOURCE schema.sql;
+//SOURCE insert-schema.sql;
+//SHOW databases;
+//SHOW tables;
+//node server.js
+//gonna use insomnia
