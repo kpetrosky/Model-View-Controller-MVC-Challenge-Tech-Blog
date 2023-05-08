4f0848e92556f429e8d55cc120181e00476786bb
kpetrosky <kpetrosky36@gmail.com>
2023-05-07 22:20:16 -0700

    added code to index.js within the models folder

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newProject = await Project.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newProject);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const projectData = await Project.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+        if (projectData) {
+            res.status(200).json({ message: 'Project deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'Project not found' });
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+module.exports = router;
