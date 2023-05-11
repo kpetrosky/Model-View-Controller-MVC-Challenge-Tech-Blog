dcabf1d23beaf2f31c8ef51eff0b91078ed90a30
kpetrosky <kpetrosky36@gmail.com>
2023-05-11 15:23:23 -0700

    trying to get server to work

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
 DROP DATABASE IF EXISTS blog_db;
 CREATE DATABASE blog_db;
+
+ CREATE TABLE posts (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    content TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+     );
