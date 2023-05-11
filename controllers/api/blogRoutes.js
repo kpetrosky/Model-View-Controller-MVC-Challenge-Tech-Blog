947e398ef01750c1a491c8f36e12e6836f131e3c
kpetrosky <kpetrosky36@gmail.com>
2023-05-11 05:55:40 -0700

    updated names with controllers/api/blog routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,34 +2,34 @@ const router = require('express').Router();
 const { Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// router.post('/', withAuth, async (req, res) => {
-//     try {
-//         const newProject = await Project.create({
-//             ...req.body,
-//             user_id: req.session.user_id,
-//         });
-//         res.status(200).json(newProject);
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const NewBlog = await NewBlog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newProject);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//     try {
-//         const projectData = await Project.destroy({
-//             where: {
-//                 id: req.params.id,
-//                 user_id: req.session.user_id,
-//             }
-//         });
-//         if (projectData) {
-//             res.status(200).json({ message: 'Project deleted successfully' });
-//         } else {
-//             res.status(404).json({ message: 'Project not found' });
-//         }
-//     } catch (error) {
-//         res.status(500).json(error);
-//     }
-// });
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const NewBlog = await NewBlog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+        if (newBlog) {
+            res.status(200).json({ message: 'NewBlog deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'NewBlog not found' });
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
