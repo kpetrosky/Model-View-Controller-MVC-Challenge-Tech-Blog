321b61fedfd40014a0fcf6488b5321d9f26b1564
kpetrosky <kpetrosky36@gmail.com>
2023-05-11 05:58:40 -0700

    updated name of varibles through homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,45 +29,45 @@ router.get('/', async (req, res) => {
 });
 
 
-// router.get('/project/:id', async (req, res) => {
-//     try {
-//         const projectData = await Project.findByPk(req.params.id, {
-//             include: [
-//                 {
-//                     model: User,
-//                     attributes: ['name'],
-//                 }
-//             ],
-//         });
+router.get('/blog/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                }
+            ],
+        });
 
-//         const project = projectData.get({ plain: true });
+        const blog = blogData.get({ plain: true });
 
-//         res.render('project', {
-//             ...project,
-//             logged_in: req.session.logged_in
-//         });
+        res.render('blog', {
+            ...blog,
+            logged_in: req.session.logged_in
+        });
 
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-// router.get('/profile', withAuth, async (req, res) => {
-// try {
-//     const userData = await User.findByPk(req.session.user_id,{
-//         attributes: { exlude: ['password'] },
-//         include: [{model: Project} ],
-//     });
-// const user = userData.get({ plain: true });
+router.get('/profile', withAuth, async (req, res) => {
+try {
+    const userData = await User.findByPk(req.session.user_id,{
+        attributes: { exlude: ['password'] },
+        include: [{model: Blog} ],
+    });
+const user = userData.get({ plain: true });
 
-// res.render('profile', {
-//     ...user,
-//     logged_in: true
-// });
-// } catch(err)  {
-//     res.status(500).json(err);
-// }
-// });
+res.render('profile', {
+    ...user,
+    logged_in: true
+});
+} catch(err)  {
+    res.status(500).json(err);
+}
+});
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
